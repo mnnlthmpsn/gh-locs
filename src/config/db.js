e1b0120a2693b8d52b12f2402e619ab11afd66de
mnnlthmpsn <mnnlthmpsn@outlook.com>
2021-03-28 21:17:40 +0000

    ssl true

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import "dotenv/config"
 
 const db = new Sequelize(
-  process.env.DATABASE_URL, {logging: false}
+  process.env.DATABASE_URL, {logging: false, ssl: true}
 )
 
 export default db
